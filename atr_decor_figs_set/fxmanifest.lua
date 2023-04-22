396c23bb8f9842b109a385e32a2264ade908d67f
ATRVIIE <119594378+ATRVIIE@users.noreply.github.com>
2023-04-22 20:15:42 +0200

    Add files via upload

---
+++ atr_decor_figs_set/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'ATR#4444' 
+
+files {
+  'stream/atr_decor_figs_set_props.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/atr_decor_figs_set_props.ytyp'
